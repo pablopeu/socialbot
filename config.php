c227c9305037d24e328d426c81e0ca8ec6fb37e5
Pablo E. Untroib <pablou@gmail.com>
2025-06-29 18:01:04 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+  'token'           => 'aca va el token del bot de telegram',
+  'TWITTER_BEARER'  => 'aca va el bearer token',
+  'LOG_FILE'        => 'bot.log',
+  'third_party_api_key' => 'aca va la api key de rapidapi',
+];
